61a43473daf19f58334dde18a56475f5228a219c
m <m@home>
2018-09-23 18:31:12 +0200

    test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,9 +6,14 @@ package com.mycompany.app;
  */
 public class App 
 {
+    private void test(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+    {
+        System.out.println( "Hello World!" );
+ 
+    }
     public static void main( String[] args )
     {
-        int i;
+        int i,j;
 
         for (i=0;i<10; i++)
         System.out.println( "Hello World!" );
