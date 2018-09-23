fe0c271e88b64f3815ffacaddb0712c85617f9e0
m <m@home>
2018-09-23 18:16:18 +0200

    test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        int i;
+
+        for (i=0;i<10; i++)
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
 }
